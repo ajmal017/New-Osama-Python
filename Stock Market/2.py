60159725d28e546eeba91c267b040b7d0d696d31
engosam69 <Os01265065()>
2020-03-31 19:10:55 +0200

    1-4-2020

---
+++ Stock Market/2.py
@@ -0,0 +1,6 @@
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np
+product = {'month': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 'demand': [290, 260, 288, 300, 310, 303, 329, 340, 316, 330, 308, 310]}
+df = pd.DataFrame(product)
+print(df)
