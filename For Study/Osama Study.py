4ba5647c522e88de35dda460fcb3d89ba724d0ac
engosam69 <Os01265065()>
2020-03-31 21:49:18 +0200

    1-4-2020

--- For Study/Osama Study.py
+++ For Study/Osama Study.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 df = pd.read_csv('D:\Python\Project\example')
 print(df)
-df.to_csv('D:\Python\Project\example',index=False)
+df.to_csv('D:\Python\Project\example', index=False)
 print(df)
-exell=pd.read_excel('D:\Python\Project\Excel_Sample.xlsx',sheet_name='Sheet1')
+exell = pd.read_excel('D:\Python\Project\Excel_Sample.xlsx', sheet_name='Sheet1')
 print(exell)
 df = pd.read_html('http://www.fdic.gov/bank/individual/failed/banklist.html')
 print(df)
